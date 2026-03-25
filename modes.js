fd08debe5263d5ca9b464dcbeac1d0e7adfcdd44
Harneet <harneet.bhalla@mail.utoronto.ca>
2026-03-25 18:06:52 -0400

    made the reccomended mode clearer by adding a glow

--- modes.js
+++ modes.js
@@ -20,30 +20,48 @@ function moveToTop(card){
   stack.prepend(card);
 }
 
+function setRecommended(card){
+  // remove from all first
+  document.querySelectorAll(".modeCard").forEach(c => {
+    c.classList.remove("recommended");
+  });
+
+  // add to selected one
+  card.classList.add("recommended");
+}
+
 if (mood === "good") {
   document.body.classList.add("theme-good");
   moodTitle.textContent = "Glad you're feeling good today!";
   moodSub.textContent = "Recommended mode: Want to keep the calm going? Try Tune Out.";
+
   moveToTop(tuneCard);
+  setRecommended(tuneCard);
 }
 
 else if (mood === "rough") {
   document.body.classList.add("theme-rough");
   moodTitle.textContent = "Hey, it’s totally okay to take a pause!";
-  moodSub.textContent = "Recommended mode: Try writing out your feelings. ";
+  moodSub.textContent = "Recommended mode: Try writing out your feelings.";
+
   moveToTop(journalCard);
+  setRecommended(journalCard);
 }
 
 else if (mood === "overwhelmed") {
   document.body.classList.add("theme-overwhelmed");
   moodTitle.textContent = "It sounds like you're feeling overwhelmed.";
   moodSub.textContent = "Recommended mode: Let's slow things down for a moment and just breathe.";
+
   moveToTop(momentCard);
+  setRecommended(momentCard);
 }
 
 else {
   document.body.classList.add("theme-neutral");
   moodTitle.textContent = "That’s okay! You don’t need to have a clear answer.";
   moodSub.textContent = "Recommended mode: Try writing out your thoughts if it helps.";
+
   moveToTop(journalCard);
+  setRecommended(journalCard);
 }
